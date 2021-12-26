218e92289790fff3a2feb68202d2b6227865c68f
bonbybao169 <bonby201@gmail.com>
2021-12-26 22:04:15 +0700

    Revert "add view Cat, del Cat admin"
    This reverts commit 9edc29c6ed5eef9fd8b9c9c62fd32367664d7b17.

--- models/category.model.js
+++ models/category.model.js
@@ -6,35 +6,11 @@ export default {
     },
 
     findParentCat() {
-        return db('category').where('ParentID', null);
+        return db('category').where('ParentID', 'null');
     },
 
     findChildCat() {
-       return db('category').where('ParentID', '<>', null);
+       return db('category').where('ParentID', '<>', 'null');
 
-    },
-
-    add(entity) {
-        return db('category').insert(entity);
-    },
-
-    async findByID(id) {
-        const list = await db('category').where('CategoryID', id);
-        console.log(list);
-        if (list.length === 0)
-            return null;
-
-        return list[0];
-    },
-
-    delete(id) {
-        return db('category').where('CategoryID', id).del();
-    },
-
-    patch(entity) {
-        const id = entity.CategoryID;
-        delete entity.CategoryID;
-
-        return db('category').where('CategoryID', id).update(entity);
     }
 }
