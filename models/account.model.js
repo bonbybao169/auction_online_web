c36c7b511a223144f8292b5dc0164f3f9b919d05
bonbybao169 <bonby201@gmail.com>
2022-01-04 23:49:10 +0700

    add update account for admin

--- models/account.model.js
+++ models/account.model.js
@@ -10,30 +10,22 @@ export default {
     },
 
     async findByID(id) {
-        const list = await db('category').where('CategoryID', id);
-        console.log(list);
+        const list = await db('user').where('Username', id);
+        // console.log(list);
         if (list.length === 0)
             return null;
 
         return list[0];
     },
 
-    async delete(id) {
-        const list = await db('product').where('Category', id);
-        const listParent = await this.findByParentID(id);
-        console.log("listP:", listParent);
-
-        if (list.length === 0 && listParent.length === 0) {
-            return db('category').where('CategoryID', id).del();
-        } else {
-            return null;
-        }
+    delete(id) {
+        return db('user').where('Username', id).del();
     },
 
     patch(entity) {
-        const id = entity.CategoryID;
-        delete entity.CategoryID;
+        const id = entity.Username;
+        delete entity.Username;
 
-        return db('category').where('CategoryID', id).update(entity);
+        return db('user').where('Username', id).update(entity);
     }
 }
--- routes/account_admin.route.js
+++ routes/account_admin.route.js
@@ -20,29 +20,28 @@ router.post('/add', async function (req, res) {
     res.redirect('/admin/accounts')
 })
 
-// router.get('/edit', async function (req, res) {
-//     const id = req.query.id || 0;
-//     const list = await accountModel.findParentCat();
-//     const category = await accountModel.findByID(id);
-//     if (category === null) {
-//         return res.redirect('/admin/categories');
-//     }
-//     res.render('vwCategory/edit', {
-//         category,
-//         accounts: list
-//     });
-// })
-//
-// router.post('/del', async function (req, res) {
-//     const ret = await accountModel.delete(req.body.CategoryID);
-//     // console.log(ret);
-//     res.redirect("/admin/categories");
-// })
-//
-// router.post('/patch', async function (req, res) {
-//     const ret = await accountModel.patch(req.body);
-//     // console.log(ret);
-//     res.redirect("/admin/categories");
-// })
+router.get('/edit', async function (req, res) {
+    const id = req.query.id || 0;
+    const account = await accountModel.findByID(id);
+    if (account === null) {
+        return res.redirect('/admin/accounts');
+    }
+    res.render('vwAccount/edit', {
+        account,
+        isSeller: account.Type === 3
+    });
+})
+
+router.post('/del', async function (req, res) {
+    const ret = await accountModel.delete(req.body.Username);
+    // console.log(ret);
+    res.redirect("/admin/accounts");
+})
+
+router.post('/patch', async function (req, res) {
+    const ret = await accountModel.patch(req.body);
+    // console.log(ret);
+    res.redirect("/admin/accounts");
+})
 
 export default router;
