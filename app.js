d993d79a921f910c9ebb3c5ac78c11e1f21b3b18
dat-badboy <64117485+dat-badboy@users.noreply.github.com>
2022-01-04 14:39:30 +0700

    fix db and login

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 import express from 'express';
 import morgan from 'morgan';
 import asyncError from 'express-async-errors'
-
+import session from "express-session";
+import mySqlSessionStore from "express-mysql-session";
 import activate_local_mdw from './middlewares/locals.mdw.js/';
 import activate_view_mdw from './middlewares/view.mdw.js';
 import activate_route_mdw from './middlewares/routes.mdw.js';
+import categoryModel from './models/category.model.js';
+import { connectionInfo } from './utils/db.js';
 
-import categoryModel from './models/category.model.js'
 
+const MySqlSession = mySqlSessionStore(session);
 const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded({
     extended: true
 }));
 app.use('/image', express.static('image'));
-
+app.set('trust proxy', 1)
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+        store: new MySqlSession(connectionInfo),
+        cookie: {
+            // secure: true
+        }
+    })
+);
 app.use(async function (req,res,next){
     res.locals.lcChildCat = await categoryModel.findChildCat();
     res.locals.lcParentCat = await categoryModel.findParentCat();
     next();
 });
 
+activate_local_mdw(app);
 activate_view_mdw(app);
 activate_route_mdw(app);
 
