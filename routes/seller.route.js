49a8af9da6501f5ffc61c9aefa77f9b39ba5a177
TuongVy0910 <63916775+TuongVy0910@users.noreply.github.com>
2022-01-08 13:19:50 +0700

    add detail of my posting product

--- routes/seller.route.js
+++ routes/seller.route.js
@@ -1,6 +1,9 @@
 import express from 'express';
 
 import productModel from "../models/product.model.js";
+import accountModel from "../models/account.model.js";
+import auctionhistoryModel from "../models/auctionhistory.model.js";
+import auctionModel from "../models/auction.model.js";
 const router = express.Router();
 router.get('/', async function(req, res) {
     res.redirect('/seller/home');
@@ -60,13 +63,65 @@ router.get('/products', async function(req, res) {
         list[i].DateExpired = productModel.distance(date);
     }
     // console.log(list);
-    res.render('vwProduct/add', {
+    res.render('vwProduct/index_seller', {
         layout: "SellerLayout.hbs",
         products: list,
         pageNumbers,
         empty: list.length === 0,
         page
     })
+})
+
+router.get('/products/:id', async function (req, res) {
+    const proID = req.params.id || 0;
+    const product = await productModel.findByID(proID);
+    const list = await productModel.findByCatIDExceptProID(product.ID, product.Category, 5);
+
+    product.isExpired = false;
+    if(productModel.timeDifference(product.DateExpired, new Date()) ==='Sản phẩm hết hạn'){
+        product.isExpired = true;
+    }
+    product.isLoved = await accountModel.isLoved(res.locals.user.Username,product.ID);
+
+    let date = new Date(product.DateUpload);
+    product.DateUpload = date.toLocaleDateString('en-GB');
+
+    if (productModel.timeDifference(product.DateExpired, new Date()) !== false) {
+        product.DateExpired = productModel.timeDifference(product.DateExpired, new Date());
+    }
+    else {
+        date = new Date(product.DateExpired);
+        product.DateExpired = date.toLocaleDateString('en-GB');
+    }
+
+    const auctionhistory = await auctionhistoryModel.findByID(product.ID);
+    for(let i=0;i<auctionhistory.length;i++){
+        date = new Date(auctionhistory[i].AuctionTime);
+        auctionhistory[i].AuctionTime = date.toLocaleString('en-GB');
+        const words = auctionhistory[i].Name.split(' ');
+        auctionhistory[i].Name="****"+words[words.length-1];
+    }
+
+    var user = await auctionModel.find(res.locals.user.Username, product.ID);
+    if(typeof (user) ==="undefined"){
+        user = {};
+        user.HighestPrice=null;
+    }else{
+        user.AuctionTime = new Date(user.AuctionTime).toLocaleString('en-GB');
+    }
+
+    user.isEligibled = false;
+    if((await accountModel.RateofSb(res.locals.user.Username))>=0.8||(product.BidderRate==0&&(await accountModel.RateofSb(res.locals.user.Username))==false)){
+        user.isEligibled=true;
+    }
+    console.log(user.isEligibled);
+    res.render('vwProduct/detail_posting_seller', {
+        layout: false,
+        product,
+        sameproducts: list,
+        auctionhistory,
+        noBidder: product.HighestBidder === null
+    });
 })
 
 router.get('/products/byCat/:id', async function (req, res) {
